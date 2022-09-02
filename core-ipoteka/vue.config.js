5cc2f30d030d5c5841804e4632dbbe48e45f18d5
sergey_zhuravlev <ser90593696.zhuravlev@yandex.ru>
2022-09-02 19:51:50 +0300

    build: added name-app plus hash for vue-app - vue config css.modules

--- core-ipoteka/vue.config.js
+++ core-ipoteka/vue.config.js
@@ -1,6 +1,13 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { defineConfig } = require('@vue/cli-service')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require('./package.json');
+
+const getAppName = () => {
+    const [, appName] = packageJson.name.split('/');
+    return appName.charAt(0).toUpperCase() + appName.slice(1, appName.length);
+}
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ?
@@ -16,4 +23,14 @@ module.exports = {
     devServer: {
         host: 'localhost',
     },
+    css: {
+        requireModuleExtension: false,
+        loaderOptions: {
+            css: {
+                modules: {
+                    localIdentName: `${getAppName()}_[name]_[local]_[hash:base64:5],`
+                },
+            }
+        }
+    }
 }
