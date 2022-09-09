3b20dad492c265b55b9fdfcae1a418e00aa0e136
sergey_zhuravlev <ser90593696.zhuravlev@yandex.ru>
2022-09-09 19:45:58 +0300

    feat: added mock data for table

--- react/src/pages/dataSteps.ts
+++ react/src/pages/dataSteps.ts
@@ -9,7 +9,7 @@ export const steps:IStep[] = [
                 key: '1',
                 name: 'John Brown',
                 description: 'New York Noss. 1 Lake Park',
-                tags: ['Средний', 'Пример'],
+                tags: ['Средний','Высокий'],
             },
             {
                 key: '2',
@@ -52,25 +52,145 @@ export const steps:IStep[] = [
     {
         title: 'Критерии квартиры',
         content: 'Second-content 2',
+        data: [
+            {
+                key: '1',
+                name: 'John Brown',
+                description: 'New York No. 1 Lake Park',
+                tags: ['Высокий', 'Пример'],
+            },
+            {
+                key: '2',
+                name: 'Jim Green',
+                description: 'London No. 1 Lake Park',
+                tags: ['Высокий'],
+            },
+            {
+                key: '3',
+                name: 'Joe Black',
+                description: 'Sidney No. 1 Lake Park',
+                tags: ['Высокий'],
+            },
+        ],
     },
     {
         title: 'Поиск квартиры',
         content: 'Last-content 3',
+        data: [
+            {
+                key: '1',
+                name: 'John Brown',
+                description: 'New York No. 1 Lake Park',
+                tags: ['Высокий', 'Пример'],
+            },
+            {
+                key: '2',
+                name: 'Jim Green',
+                description: 'London No. 1 Lake Park',
+                tags: ['Высокий'],
+            },
+            {
+                key: '3',
+                name: 'Joe Black',
+                description: 'Sidney No. 1 Lake Park',
+                tags: ['Высокий'],
+            },
+        ],
     },
     {
         title: 'Выбор банка',
         content: 'Last-content 4',
+        data: [
+            {
+                key: '1',
+                name: 'John Brown',
+                description: 'New York No. 1 Lake Park',
+                tags: ['Высокий', 'Пример'],
+            },
+            {
+                key: '2',
+                name: 'Jim Green',
+                description: 'London No. 1 Lake Park',
+                tags: ['Высокий'],
+            },
+            {
+                key: '3',
+                name: 'Joe Black',
+                description: 'Sidney No. 1 Lake Park',
+                tags: ['Высокий'],
+            },
+        ],
     },
     {
         title: 'Сбор документов',
         content: 'Last-content 5',
+        data: [
+            {
+                key: '1',
+                name: 'John Brown',
+                description: 'New York No. 1 Lake Park',
+                tags: ['Высокий', 'Пример'],
+            },
+            {
+                key: '2',
+                name: 'Jim Green',
+                description: 'London No. 1 Lake Park',
+                tags: ['Высокий'],
+            },
+            {
+                key: '3',
+                name: 'Joe Black',
+                description: 'Sidney No. 1 Lake Park',
+                tags: ['Высокий'],
+            },
+        ],
     },
     {
         title: 'Одобрение банка',
         content: 'Одобрение банка',
+        data: [
+            {
+                key: '1',
+                name: 'John Brown',
+                description: 'New York No. 1 Lake Park',
+                tags: ['Высокий', 'Пример'],
+            },
+            {
+                key: '2',
+                name: 'Jim Green',
+                description: 'London No. 1 Lake Park',
+                tags: ['Высокий'],
+            },
+            {
+                key: '3',
+                name: 'Joe Black',
+                description: 'Sidney No. 1 Lake Park',
+                tags: ['Высокий'],
+            },
+        ],
     },
     {
         title: 'Документы на квартиру',
         content: 'Одобрение банка',
+        data: [
+            {
+                key: '1',
+                name: 'John Brown',
+                description: 'New York No. 1 Lake Park',
+                tags: ['Высокий', 'Пример'],
+            },
+            {
+                key: '2',
+                name: 'Jim Green',
+                description: 'London No. 1 Lake Park',
+                tags: ['Высокий'],
+            },
+            {
+                key: '3',
+                name: 'Joe Black',
+                description: 'Sidney No. 1 Lake Park',
+                tags: ['Высокий'],
+            },
+        ],
     },
 ];
